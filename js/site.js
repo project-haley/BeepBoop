5694876d30f9537fdc5cb3d2ed83f05eb413182f
project-haley <60005425+project-haley@users.noreply.github.com>
2021-08-27 21:38:25 -0500

    generated repository

--- js/site.js
+++ js/site.js
@@ -1,5 +1,21 @@
+function getString() {
+    var targetString = document.getElementById("targetString").value;
+    return targetString;
+}
 
-// call Hello World
-function HelloWorld() {
-    alert("Hello World!")
+function reverseString() {
+    var targetString = getString();
+    var targetStringReverse = ""
+
+    for(let i = targetString.length - 1; i >= 0; i--) {
+        targetStringReverse += targetString[i];
+    };
+
+    return targetStringReverse;
+}
+
+function printReverseString() {
+    var printHeader = document.getElementById("results");
+    
+    printHeader.innerHTML = reverseString();
 }
