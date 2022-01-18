85e68304fb5ea4292bb4b021b1c92436ba2914df
project-haley <60005425+project-haley@users.noreply.github.com>
2022-01-18 10:42:22 -0600

    finishing touches

--- js/site.js
+++ js/site.js
@@ -1,21 +1,42 @@
-function getString() {
-    var targetString = document.getElementById("targetString").value;
-    return targetString;
+function getValues() {
+    let firstValue = document.getElementById("firstValue").value;
+    let secondValue = document.getElementById("secondValue").value;
+
+    firstValue = parseInt(firstValue);
+    secondValue = parseInt(secondValue);
+
+    if (Number.isInteger(firstValue) && Number.isInteger(secondValue)) {
+        let numbers = [firstValue, secondValue];
+        return numbers;
+    }
+
+    // check for zeros, negatives
 }
 
-function reverseString() {
-    var targetString = getString();
-    var targetStringReverse = ""
+function generateTable() {
+    let multiples = getValues();
+    let tableResult = document.getElementById("results")
+    let tableString = ""
+    let className = "noMultiple"
+    let beepBoopString = "beep"
 
-    for(let i = targetString.length - 1; i >= 0; i--) {
-        targetStringReverse += targetString[i];
-    };
+    for (index = 1; index <= 100; index++) {
+        if (index % multiples[0] == 0 && index % multiples[1] != 0) {
+            className = "firstMultiple";
+            beepBoopString = "beep";
+        } else if (index % multiples[1] == 0 && index % multiples[0] != 0) {
+            className = "secondMultiple";
+            beepBoopString = "boop";
+        } else if (index % multiples[1] == 0 && index % multiples[0] == 0) {
+            className = "bothMultiple";
+            beepBoopString = "beepboop";
+        } else {
+            className = "noMultiple";
+            beepBoopString = `${index}`;
+        }
+        tableString += `<tr><td class="${className}">${beepBoopString}</td></tr>`;
 
-    return targetStringReverse;
-}
+    }
 
-function printReverseString() {
-    var printHeader = document.getElementById("results");
-    
-    printHeader.innerHTML = reverseString();
+    tableResult.innerHTML = tableString;
 }
